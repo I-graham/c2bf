27d836ac3c2de39c58342450db3be6e0e5eb3d44
Ian Graham Martinez <iangraham28@gmail.com>
2025-09-07 12:26:32 -0400

    Fixed Calling Convention

--- c_tests/simple.c
+++ c_tests/simple.c
@@ -1,10 +1,14 @@
 char r = 'A'-1;
 char h = r + 4;
-char i = r + 4*2 + 1;
-int main() {
-  putchar(h + 4);
+char i = r + 4*2;
+
+int inner() {
+  i++;
   putchar(i);
+}
+
+int main() {
   h = h + 4;
   putchar(h);
-  putchar(i);
+  inner();
 }
